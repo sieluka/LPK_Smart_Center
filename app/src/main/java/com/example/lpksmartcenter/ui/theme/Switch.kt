3f3b79340c2b2eda4c632b819abed92370b3cea3
sieluka <lukasz.sieradzkizsk@gmail.com>
2025-12-30 15:28:55 +0100

    Czyszczenie kodu

--- app/src/main/java/com/example/lpksmartcenter/ui/theme/Switch.kt
+++ app/src/main/java/com/example/lpksmartcenter/ui/theme/Switch.kt
@@ -8,13 +8,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun SwitchMinimalExample(
