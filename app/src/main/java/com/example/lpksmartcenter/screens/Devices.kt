e7303bd818883a30cafbc916e001c139f12d37f1
sieluka <lukasz.sieradzkizsk@gmail.com>
2026-01-20 19:23:05 +0100

    Zaktualizowanie paddingu i odstępów w LazyColumn w Devices.kt

--- app/src/main/java/com/example/lpksmartcenter/screens/Devices.kt
+++ app/src/main/java/com/example/lpksmartcenter/screens/Devices.kt
@@ -40,9 +40,9 @@ fun DevicesScreen(
     LazyColumn(
         modifier = modifier
             .fillMaxSize(),
-        contentPadding = PaddingValues(50.dp),
+        contentPadding = PaddingValues(top = 50.dp, start = 32.dp, end = 32.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
     ) {
 
         items(filteredDevices.entries.toList()) { (key, isOn) ->
